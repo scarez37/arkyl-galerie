c766c13799ae3fd26a5c7c8dd519324a2a28e5f0
Z Scare7 <scarez37@gmail.com>
2026-03-11 07:00:02 +0000

    Update migration_is_sold.sql

--- migration_is_sold.sql
+++ migration_is_sold.sql
@@ -1,7 +1,7 @@
--- Ajouter la colonne is_sold à la table artworks
-ALTER TABLE artworks ADD COLUMN IF NOT EXISTS is_sold BOOLEAN DEFAULT FALSE;
+-- Migration : ajout colonne weight_g (poids en grammes) pour calcul frais de port La Poste
+ALTER TABLE artworks ADD COLUMN IF NOT EXISTS weight_g INTEGER DEFAULT 0;
 
 -- Vérification
 SELECT column_name, data_type, column_default 
 FROM information_schema.columns 
-WHERE table_name = 'artworks' AND column_name = 'is_sold';
+WHERE table_name = 'artworks' AND column_name = 'weight_g';
